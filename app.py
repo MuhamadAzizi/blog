23f789650271c9bfdff524720f560080bcdfc988
Muhamad Azizi <muhamadazizi188@gmail.com>
2022-09-29 03:18:44 +0700

    Login handling

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, session
+from flask_session import Session
+from models import login_auth, get_all_users
+
+import hashlib
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SESSION_PERMANTENT'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
 
 
 @app.route('/')
@@ -8,9 +16,43 @@ def index():
     return 'hello world'
 
 
-@app.route('/admin/login')
+@app.route('/admin')
+def admin_dashboard():
+    return render_template('Admin/index.html')
+
+
+@app.route('/admin/users')
+def admin_users():
+    users = get_all_users()
+    return render_template('Admin/Users/index.html', users=users)
+
+
+@app.route('/admin/login', methods=['POST', 'GET'])
 def admin_login():
+    if request.method == 'POST':
+        email_address = request.form['email_address']
+        password = request.form['password']
+
+        md5 = hashlib.md5()
+        md5.update(password.encode('utf-8'))
+
+        result = login_auth(email_address, md5.hexdigest())
+        if result:
+            session['name'] = result['name']
+            session['email_address'] = result['email_address']
+            session['user_level'] = result['user_level']
+            return redirect('/admin')
+        else:
+            return render_template('login.html', error='Invalid email or password')
     return render_template('login.html')
+
+
+@app.route('/admin/logout')
+def admin_logout():
+    session['name'] = None
+    session['email_address'] = None
+    session['user_level'] = None
+    return redirect('/admin/login')
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,29 +1,51 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(
-    user='root', password='',
-    host='localhost', database='blog')
-cursor = cnx.cursor()
+
+def start_connection():
+    cnx = mysql.connector.connect(
+        user='root', password='',
+        host='localhost', database='blog')
+    return cnx
+
 
 def get_all_users():
     query = ('SELECT * FROM users')
-    cursor.query(query)
 
-    result = {
-        'id': [],
-        'name': [],
-        'email_address': [],
-        'password': [],
-        'user_level': []
-    }
+    cnx = start_connection()
+    cursor = cnx.cursor()
+    cursor.execute(query)
+
+    result = []
     for id, name, email_address, password, user_level in cursor:
-        result['id'].append(id)
-        result['name'].append(name)
-        result['email_address'].append(email_address)
-        result['password'].append(password)
-        result['user_level'].append(user_level)
+        row = []
+        row.append(id)
+        row.append(name)
+        row.append(email_address)
+        row.append(password)
+        row.append(list(user_level)[0])
+        result.append(row)
 
     cursor.close()
+    cnx.close()
     return result
 
-cnx.close()
+
+def login_auth(email_address, password):
+    query = (
+        f"SELECT * FROM users WHERE email_address = '{email_address}' AND password = '{password}'")
+
+    cnx = start_connection()
+    cursor = cnx.cursor()
+    cursor.execute(query)
+
+    result = {}
+    for id, name, email_address, password, user_level in cursor:
+        result['id'] = id
+        result['name'] = name
+        result['email_address'] = email_address
+        result['password'] = password
+        result['user_level'] = user_level
+
+    cursor.close()
+    cnx.close()
+    return result
